037a2ae9c5d0c860508e57f27e401deb717bfe99
attoum8643f <ibrahim.attoumani@etu.umontpellier.fr>
2024-04-08 00:26:13 +0200

    équivalence entre les tableaux juxtaposée et la matrice dépliée

--- programme/exemple.R
+++ programme/exemple.R
@@ -1,19 +1,16 @@
 mat1 = matrix(
   c(1,-1,2,
-    0, 1,2), nrow = 2, byrow = TRUE
-)
+    0, 1,2), nrow = 3)
 mat1
 
 mat2 = matrix(
   c(2,-1,0,
-    -1, 0,1), nrow = 2, byrow = TRUE
-)
+    -1, 0,1), nrow = 3)
 mat2
 
 mat3 = matrix(
   c(3,1,0,
-    1, -2,-3), nrow = 2, byrow = TRUE
-)
+    1, -2,-3), nrow = 3)
 mat3
 
 tab_mat = list(mat1, mat2, mat3)
@@ -24,31 +21,36 @@ result_mat
 result_prop_mat = eigen(result_mat)# list ayant les valeurs et vecteurs propres
 val_prop_mat = result_prop_mat$values #valeurs propres
 vec_prop_mat = result_prop_mat$vectors #vecteurs propres associées
-for (i in 1:3) {
-  for (j in 1:3) {
-    p <- t(vec_prop_mat[,i]) %*% vec_prop_mat[,j]
-    print(paste("Le produit scalaire entre le vecteur", i, "et le vecteur", j, "est:", p))
-  }
-}
 
-
-mat_d = matrix(
+# Données
+mat_d <- matrix(
   c( 1, 2, 3,
-     0,-1, 1,
-    -1,-1, 1,
-     1, 0,-2,
+     -1,-1, 1,
      2, 0, 0,
-     2, 1, 3 
+     0,-1, 1,
+     1, 0,-2,
+     2, 1,-3
   ), nrow = 6, byrow = TRUE
 )
 
-mat_d = scale(mat_d, center = T, scale = T)*sqrt(6/5)
-mat_dep = t(mat_d)%*%mat_d
-mat_dep
 
-result_prop_dep = eigen(mat_dep)# list ayant les valeurs et vecteurs propres
-val_prop_dep = result_prop_dep$values #valeurs propres
-vec_prop_dep = result_prop_dep$vectors
+oper_inert = function(X){
+  
+  p = length(X[1,]) # la dimension des individus
+  n = length(X[,1]) # la dimension des variables
+  W = diag(n)/n
+  M = matrix(0, nrow = p, ncol = p)
+  
+  for (i in 1:p) {
+    M[i,i] = 1/sqrt(t(X[,i])%*%W%*%X[,i])
+  }
+  return(M %*% t(mat_d) %*% W %*% X %*% M)
+}
 
+# Calcul de la matrice
+mat_dep <- oper_inert(mat_d)
 
-print(data.frame(vec_prop))
+# Calcul des valeurs propres et vecteurs propres
+result_prop_dep <- eigen(mat_dep)
+val_prop_dep <- result_prop_dep$values
+vec_prop_dep <- result_prop_dep$vectors
